a6051197949445cc56ee84c11d340c4fc3be1b52
Andrés Castillo <andres.castillo@acastillo.co>
2022-12-15 20:20:33 -0500

    javascript operadores ternarios, swith, operadores logicos

--- main.js
+++ main.js
@@ -71,3 +71,133 @@ if (marcosMayorImc) {
 } else {
   console.log('marcos tiene menor IMC que Juan')
 }
+
+const number1 = parseInt("12");
+const number3 = Number("12");
+console.log(number1)
+
+const number2 = parseInt("abc");
+console.log(number2)
+console.log(typeof number2);
+
+const toString1 = 12 + '';
+console.log(toString1);
+console.log(typeof toString1);
+console.log(typeof toString1 === 'string');
+
+const toString2 = String(12);
+console.log(toString2);
+console.log(typeof toString2);
+console.log(typeof toString2 === 'string');
+
+Number.parseInt("13", 10);
+String.toString(15);
+
+// Truthy - Falsey
+// Truthy: 12, 'dasd', true, {}, []
+if (number1) {
+  console.log('number1 es truthy')
+}
+// Falsey: '', undefined, null, 0, false
+let variable1;
+const obj = null;
+if (variable1) {
+  console.log('variable1 es truthy')
+}
+
+// == y ===
+console.log(1 === 1);
+console.log(1 === 2);
+console.log(1 === '1');
+console.log(1 == '1');
+console.log(1 + '1');
+console.log('1' + 1);
+console.log(1 + 1 + '1');
+console.log('1' + 1 + 1);
+console.log(+'1' + 1 + 1);
+console.log('10' - 5);
+console.log(5 - '10');
+
+let strinconc = '12345';
+strinconc += '67890'; // stringconc = stringconc + '67890'
+console.log(strinconc)
+
+let count = 0;
+console.log(++count);
+console.log(count++);
+
+console.log(true && false);
+console.log(true && true);
+
+console.log(true || false);
+console.log(true || true)
+console.log(false || false)
+
+count = 2000
+if (!count || (count < 10 && count > 5)) {
+  console.log('count esta entre 5 y 10 o no esta definido');
+} else if (count >= 10 && count < 100) {
+  console.log('count es mayor o igual a 10 y menor que 100')
+} else if (count >= 100 && count < 1000) {
+  console.log('count es mayor o igual a 100 y menor que 1000')
+} else {
+  console.log('count es mayor o igual a 1000')
+}
+
+// challenge 3
+//const puntajeDelfines = (96 + 108 + 89) / 3;
+//const puntajeKoalas = (88 + 91 + 110) / 3;
+
+// const puntajeDelfines = (97 + 112 + 101) / 3;
+// const puntajeKoalas = (109 + 95 + 123) / 3;
+
+const puntajeDelfines = (97 + 112 + 101) / 3;
+const puntajeKoalas = (109 + 95 + 106) / 3;
+
+if (puntajeDelfines >= 100 && puntajeDelfines > puntajeKoalas) {
+  console.log('gana Delfines');
+} else if (puntajeKoalas >= 100 && puntajeKoalas > puntajeDelfines) {
+  console.log('gana Koalas');
+} else if (puntajeDelfines >= 100 && puntajeDelfines === puntajeKoalas) {
+  console.log('empate');
+} else {
+  console.log('nadie gana');
+}
+
+// switch
+const edad = 19;
+switch (edad) {
+  case 18:
+    console.log('tiene 18 años case 1');
+    break;
+  case 22:
+    console.log('tiene 22 años case 2');
+    break;
+  case 50:
+    console.log('tiene 50 años case 3');
+    break;
+  default:
+    console.log(`tiene ${edad} años`);
+}
+
+let resultado;
+if (edad >= 18) {
+  if (edad <= 20) {
+    resultado = 'menor de 20';
+  } else {
+    resultado = 'mayor de edad';
+  }
+} else {
+  resultado = 'menor de edad';
+}
+console.log(resultado);
+
+resultado = edad >= 18 ? 'mayor de edad' : 'menor de edad';
+console.log(resultado);
+
+// Challenge 3
+const valorFactura = 275;
+const propina = valorFactura >= 50 && valorFactura <= 300
+  ? valorFactura * 0.15
+  : valorFactura * 0.2;
+console.log(`El valor de la factura es ${valorFactura}, la propina es ${propina} y el valor total es ${valorFactura + propina}`)
